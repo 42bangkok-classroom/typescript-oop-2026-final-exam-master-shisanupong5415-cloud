8e7b4a80b778e4a803c7f8ff73ea7be746d03302
event <event@z2t18c1.42bangkok.com>
2026-03-25 11:40:14 +0700

    p0

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,7 +6,13 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getHello(): any {
+    return this.appService.getHello(
+      true,
+      {service : "purchase-api",
+        version: "1.0.0"
+      },
+      "message"
+    );
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,12 @@
 import { Injectable } from '@nestjs/common';
-
+import { ApiResponse } from './interfaces/response.interface';
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello NestJS';
+  getHello<T>(success: boolean, data: any = null, message:string): ApiResponse<T> {
+    return {
+      success,
+      message,
+      data,
   }
+}
 }
